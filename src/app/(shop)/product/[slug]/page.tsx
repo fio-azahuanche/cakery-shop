f0d20d8ee2f3e2223d985f98abd322a9e47c0500
Fiorela <fazahuanchef@gmail.com>
2024-09-20 22:18:32 -0500

    agregar pantalla de producto

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,7 +1,41 @@
-export default function() {
+import { initialData } from "@/seed/seed";
+import notFound from "../not-found";
+import { titleFont } from "@/config/fonts";
+
+interface Props {
+    params: {
+        slug: string;
+    }
+}
+
+export default function({params}: Props) {
+
+    const { slug } = params;
+    const product = initialData.products.find(product => product.slug === slug);
+
+    if( !product ) {
+        notFound();
+    }
+
     return (
-        <div>
-            <h1>Product Page</h1>
+        <div className="mt-5 mb-20 grid grid-cols-1 md:grid-cols-3 gap-3">
+
+            <div className="col-span-1 md:col-span-2">
+
+            </div>
+            <div className="col-span-1 px-5">
+                <h1 className={`${titleFont.className} antialiased font-bold text-xl`}>{product?.title}</h1>
+                <p className="text-lg mb-5">{product?.price}</p>
+                <button className="btn-primary my-5"
+                >
+                    Agregar al carrito
+                </button>
+                <h3 className="font-bold text-sm">Descripción</h3>
+                <p className="font-light">
+                    {product?.description}
+                </p>
+            </div>
+
         </div>
     )
 };
