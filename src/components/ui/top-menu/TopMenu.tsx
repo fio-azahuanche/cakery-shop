019ef592a2896337d17b8bb6bcd242c5a2f5181f
Fiorela <fazahuanchef@gmail.com>
2024-09-20 21:22:33 -0500

    agregar menu lateral y zustand

--- src/components/ui/top-menu/TopMenu.tsx
+++ src/components/ui/top-menu/TopMenu.tsx
@@ -1,8 +1,13 @@
+'use client';
+
 import { titleFont } from "@/config/fonts"
+import { useUIStore } from "@/store"
 import Link from "next/link"
 import { IoCartOutline, IoSearchOutline } from "react-icons/io5"
 
 export const TopMenu = () => {
+    const openMenu = useUIStore( state => state.openSideMenu )
+
     return (
         <nav className="flex ox-5 justify-between items-center w-full">
             <div>
@@ -38,7 +43,7 @@ export const TopMenu = () => {
                     </div>
                 </Link>
 
-                <button className="m-2 p-2 rounded-md transition-all hover:bg-gray-100">
+                <button className="m-2 p-2 rounded-md transition-all hover:bg-gray-100" onClick={ openMenu }>
                     Menú
                 </button>
             </div>
